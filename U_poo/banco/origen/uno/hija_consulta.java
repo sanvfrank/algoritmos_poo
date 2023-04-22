300872fb28886ebb78d24c1a301abf4fff091c0a
shadiagit <131499512+shadiagit@users.noreply.github.com>
2023-04-21 19:20:02 -0500

    Update and rename U_poo/banco/src/uno/hija_consulta.java to U_poo/banco/origen/uno/hija_consulta.java

--- U_poo/banco/origen/uno/hija_consulta.java
+++ U_poo/banco/origen/uno/hija_consulta.java
@@ -19,7 +19,7 @@ public class hija_consulta extends padre {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
    
- 
+ njjkkkjk
 
    
 }
